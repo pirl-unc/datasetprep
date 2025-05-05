1913088710ba0c5a316b60578f8f4704e55e66f9
Nathan Wheeler <nwheeler@email.unc.edu>
2025-05-05 09:58:48 -0400

    Added Chemo_Rx and Chemo_Tx to patient columns

--- R/configuration_methods.R
+++ R/configuration_methods.R
@@ -54,7 +54,7 @@ get_patient_columns = function(){
     'Race', 'Caucasian', 'Asian', 'African', 'Native_American', 'Pacific_Islander', 'Likely_Caucasian',
     'ICI_Rx', 'aPD1_Rx', 'aCTLA4_Rx', 'ICI_Pathway', 'ICI_Target',
     'Non_ICI_Rx', 'NeoICI_Rx',  'ICI_Tx', 'aPD1_Tx', 'aCTLA4_Tx', 'aCTLA4_aPD1_Tx',
-    'Non_ICI_Tx', 'NeoICI_Tx',
+    'Non_ICI_Tx', 'NeoICI_Tx', 'Chemo_Rx', 'Chemo_Tx',
     'Prior_Rx', 'Prior_ICI_Rx', 'Prior_Tx', 'Prior_ICI_Tx', 'Prior_aCTLA4_Tx', 'Prior_aMAPK_Tx',
     'Subsq_Rx', 'Subsq_ICI_Rx', 'Subsq_Tx', 'Subsq_ICI_Tx', 'Subsq_aCTLA4_aPD1_Tx', 'Subsq_aCTLA4_Tx',
     'Subsq_aMAPK_Tx',
